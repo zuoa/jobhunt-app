997e14e9a40ce23320ff403b6a0c8aeace480802
yujian <yujian@airimos.com>
2024-06-22 14:29:57 +0800

    1

---
+++ src/pages/resume/index.vue
@@ -0,0 +1,113 @@
+<template>
+  <view class="content">
+    <view class="big-button" @click="onClick">
+      <view>
+        <text class="title" @click="onClick">创建简历</text>
+      </view>
+      <text class="sub-title">海量专业模版，随意创建个性化简历</text>
+    </view>
+
+
+    <view class="big-button" @click="chooseFile">
+      <view>
+        <text class="title">导入简历</text>
+      </view>
+      <text class="sub-title">支持PDF、Word格式简历，90%以上内容识别率，自动排版</text>
+    </view>
+
+    <text class="sub-title">{{title}}</text>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Hello'
+    }
+  },
+  onLoad() {
+
+  },
+  methods: {
+    onClick: function (e) {
+      console.log(11111111111111)
+      this.title="111212"
+    },
+
+    chooseFile:function (e) {
+      let that = this;
+      wx.chooseMessageFile({
+        count: 1, //限制选择的文件数量
+        type: 'file',//非图片和视频的文件,不选默认为all
+        extension:['xls', 'xlsx'],//此处限制文件类型
+        // extension: ['xls', 'xlsx'], //此处限制文件类型
+        success(res) {
+          const tempFilePaths = res.tempFiles
+          console.log('临时路径', tempFilePaths)
+          //下面是返回后需要调接口的操作
+          // 		uni.uploadFile({
+          // 			url: 'https://******/index/Api/upload',
+          // 			filePath: tempFilePaths[0].path,
+          // 			name: 'file_',
+
+          // 			formData: {
+
+          // 			},
+          // 			success: (uploadFileRes) => {
+          // 				//接口返回的数据
+          // 				console.log(uploadFileRes.data);
+          // 				that.$token.toast("上传成功", 2000)
+          // 				that.file_name = uploadFileRes.data
+          // 			}
+          // 		});
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin: 200rpx auto 50rpx auto;
+}
+
+.big-button {
+  width: 90%;
+  margin: 2%;
+  height: 200rpx;
+  background: #F9F9F9;
+
+  padding: 3%;
+  border: 1px solid #ddd;
+  border-radius: 8rpx;
+  box-shadow: 0 5rpx 15rpx #eee;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 48rpx;
+  color: #333;
+}
+
+.sub-title {
+
+  font-size: 28rpx;
+  color: #999;
+}
+
+</style>
---
+++ src/pages/vip/index.vue
@@ -0,0 +1,150 @@
+<template>
+  <view class="content">
+    <image class="banner" mode="aspectFill" src="http://iph.href.lu/879x200?fg=666666&bg=cccccc"></image>
+    <view class="description-box">
+      <rich-text :nodes="description"></rich-text>
+    </view>
+
+    <button type="primary" size="large" plain>定制专属VIP服务</button>
+
+    <view class="teacher-box">
+      <view class="box">
+        <text class="box-title">推荐导师</text>
+      </view>
+      <scroll-view scroll-x="true" class="scroll-view_H">
+        <view id="demo1" class="scroll-view-item_H uni-bg-red">
+          <image class="avatar" src="/static/image/bb1.jpg"></image>
+          <view>
+            <text class="person-name">张大有</text>
+          </view>
+          <view>
+            <text class="person-title">字节跳动资深HR</text>
+          </view>
+        </view>
+        <view id="demo1" class="scroll-view-item_H uni-bg-red">
+          <image class="avatar" src="/static/image/bb1.jpg"></image>
+          <view>
+            <text class="person-name">张大有</text>
+          </view>
+          <view>
+            <text class="person-title">字节跳动资深HR</text>
+          </view>
+        </view>
+        <view id="demo1" class="scroll-view-item_H uni-bg-red">
+          <image class="avatar" src="/static/image/bb1.jpg"></image>
+          <view>
+            <text class="person-name">张大有</text>
+          </view>
+          <view>
+            <text class="person-title">字节跳动资深HR</text>
+          </view>
+        </view>
+        <view id="demo1" class="scroll-view-item_H uni-bg-red">
+          <image class="avatar" src="/static/image/bb1.jpg"></image>
+          <view>
+            <text class="person-name">张大有</text>
+          </view>
+          <view>
+            <text class="person-title">字节跳动资深HR</text>
+          </view>
+        </view>
+      </scroll-view>
+
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Hello',
+      description: '<div class="title"><h2>你为什么需要一份好的简历</h2><br>企业HR平均每天查看上千份简历<br> 平均每份简历只看6秒钟<br>只有一份规范、专业的简历才能引起HR的注意</div>'
+    }
+  },
+  onLoad() {
+
+  },
+  methods: {}
+}
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.banner {
+  height: 280rpx;
+  width: 100%;
+
+}
+.box {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.box-title {
+  font-size: 36rpx;
+  color: #333;
+  font-weight: bold;
+  margin: 16rpx 0;
+  text-align: center;
+  border-bottom: 3px solid #393;
+}
+
+.description-box {
+  text-align: center;
+  margin: 32rpx 0;
+}
+
+.scroll-Y {
+  height: 300rpx;
+}
+
+
+.teacher-box {
+  margin: 32rpx 0;
+}
+
+.scroll-view_H {
+  white-space: nowrap;
+  width: 100%;
+}
+
+.scroll-view-item {
+  height: 300rpx;
+  line-height: 300rpx;
+  text-align: center;
+  font-size: 36rpx;
+}
+
+.scroll-view-item_H {
+  display: inline-block;
+  width: 33%;
+  text-align: center;
+  font-size: 36rpx;
+}
+
+.avatar {
+  width: 160rpx;
+  height: 160rpx;
+  border-radius: 50%;
+  margin: 16rpx auto;
+}
+
+.person-name {
+  font-size: 48rpx;
+  color: #333;
+  font-weight: bold;
+}
+
+.person-title {
+  font-size: 24rpx;
+  color: #999;
+}
+</style>
